0ee5223e3bf79b2a8cf7f66f84c3fb1faf59c3b7
micodian <micodian83@gmail.com>
2021-02-20 13:54:17 -0600

    item_db file created

---
+++ Assignment_4/model/item_db.php
@@ -0,0 +1,53 @@
+<?php
+function get_items_by_category($category_id){
+    global $db;
+    $query = 'SELECT * FROM todoitems
+                         WHERE todoitems.categoryID= :category_id
+                             ORDER BY categoryID';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':category_id',$category_id);
+    $statement->execute();
+    $items = $statement->fetchAll();
+    $statement->closeCursor();
+    return $items;
+}
+
+function get_items($itemNum){
+    global $db;
+    $query = 'SELECT * FROM todoitems
+                         WHERE itemNum= :itemNum';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':itemNum',$itemNum);
+    $statement->execute();
+    $items = $statement->fetch();
+    $statement->closeCursor();
+    return $items;
+}
+
+function delete_items($itemNum){
+    global $db;
+    //count to flag item deleted to be returned 
+    $query = 'DELETE FROM todoitems
+              WHERE itemNum = :itemNum';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':itemNum', $itemNum);
+    $success = $statement->execute();
+    $statement->closeCursor(); 
+}
+
+function add_items($itemNum){
+    global $db;
+    //count to flag item deleted to be returned 
+    $query = 'INSERT INTO todoitems
+                 (categoryID, Title, Description)
+              VALUES
+                 (:category_id, :title, :description)';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':category_id', $category_id);
+    $statement->bindValue(':title', $title);
+    $statement->bindValue(':description', $description);
+    $statement->execute();
+    $statement->closeCursor(); 
+}
+
+?>
